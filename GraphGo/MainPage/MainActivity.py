8ff131ff636ce3dc98c41bc6016395c30301f762
TerenceLiu2 <741318489@qq.com>
2018-06-04 13:29:32 +0800

    all Project

--- GraphGo/MainPage/MainActivity.py
+++ GraphGo/MainPage/MainActivity.py
@@ -17,7 +17,8 @@ class MainWait:
             info_dict['item_list']=[]
             for item in item_list:
                 info_dict['item_list'].append({'user_id':item[0],'username':item[2],'content':item[1],'img_url':"http://%s/Img/%d/profile" % (
-                    Config.Config.ip, item[0]),'activity_id':item[3]})
+                    Config.Config.ip, item[0]),'activity_id':item[3],'status':item[4]})
+            print info_dict
             return LittleTools.MakeJson(200,info_dict)
         except Exception,e:
             print e
@@ -36,7 +37,7 @@ class MainFinish:
                 info_dict['item_list'].append({'user_id':item[0],'username':item[2],'content':item[1],'profile_url':"http://%s/Img/%d/profile"% (
                 Config.Config.ip, item[0]), 'img_url': "http://%s/Img/%d/%d/0" % (
                 Config.Config.ip, item[0], int(item[3])),'activity_id':item[3]})
-            print info_dict
+
             return LittleTools.MakeJson(200,info_dict)
         except Exception,e:
             print e

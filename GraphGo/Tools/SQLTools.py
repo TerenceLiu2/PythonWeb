c7ff65989eb9e46a0eadf2d1d861f4c435d5e0b2
TerenceLiu2 <741318489@qq.com>
2018-06-04 16:21:21 +0800

    all Project

--- GraphGo/Tools/SQLTools.py
+++ GraphGo/Tools/SQLTools.py
@@ -48,8 +48,14 @@ def GetActivityFromSql(type,page_num,item_num):
 
     global cur, conn
     if type==0:
-        cur.execute("select activity.user_id,content,username,activity_id,status from activity,user where (status=0 or status=1) and user.user_id=activity.user_id limit %d,%d ;"%((page_num-1)*item_num,item_num))
+        if page_num==0:
+            cur.execute("select activity.user_id,content,username,activity_id,status from activity,user where (status=0 or status=1) and user.user_id=activity.user_id;")
+        else:
+            cur.execute("select activity.user_id,content,username,activity_id,status from activity,user where (status=0 or status=1) and user.user_id=activity.user_id limit %d,%d ;"%((page_num-1)*item_num,item_num))
         return cur.fetchall()
     else:
-        cur.execute("select activity.user_id,content,username,activity_id from activity,user where status=2 and user.user_id=activity.user_id limit %s,%s ;"%((page_num-1)*item_num,item_num))
+        if page_num==0:
+            cur.execute("select activity.user_id,content,username,activity_id,status from activity,user where (status=0 or status=1) and user.user_id=activity.user_id;")
+        else:
+            cur.execute("select activity.user_id,content,username,activity_id from activity,user where status=2 and user.user_id=activity.user_id limit %s,%s ;"%((page_num-1)*item_num,item_num))
         return cur.fetchall()

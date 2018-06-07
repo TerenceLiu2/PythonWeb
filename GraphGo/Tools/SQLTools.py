c42d3dfde7b033b946b147fea8f2989ff5901fd0
TerenceLiu2 <741318489@qq.com>
2018-06-07 22:00:15 +0800

    all Project

--- GraphGo/Tools/SQLTools.py
+++ GraphGo/Tools/SQLTools.py
@@ -137,8 +137,7 @@ def GetActivityFromSql(type,page_num,item_num):
 
 def GetNoticeFromSql(user_id):
     global cur,conn
-    print "select content,username,type from notice,activity,user where creater_id=%s and activity.activity_id=notice.activity_id and user.user_id=notice.attender_id;" % user_id
-    cur.execute("select content,username,type from notice,activity,user where creater_id=%s and activity.activity_id=notice.activity_id and user.user_id=notice.attender_id;"%user_id)
+    cur.execute("select content,username,notice.type from notice,activity,user where creater_id=%s and activity.activity_id=notice.activity_id and user.user_id=notice.attender_id;"%user_id)
 
     notice_list=[]
     for item in cur.fetchall():

15bbece3d5aa345b1c24af7c769299e74363bdce
TerenceLiu2 <741318489@qq.com>
2018-06-07 12:43:33 +0800

    all Project

--- GraphGo/Activity/FinishActivity.py
+++ GraphGo/Activity/FinishActivity.py
@@ -16,14 +16,14 @@ class FinishActivity:
         if user_id==False:
             return LittleTools.MakeJson(400,"")
         try:
-            SQLTools.UpdateSql("update activity set status=2 where activity_id=%s"%activity_id)
-            SQLTools.UpdateSql("update activity set content='%s' where activity_id=%s"%(content,activity_id))
             for i in range(size):
                 LittleTools.Mkdir("Img/%s/%s/"%(user_id,activity_id))
                 img_dir="Img/%s/%s/%d"%(user_id,activity_id,i)
                 with open(img_dir) as wf:
                     wf.write(info["p"+str(i)])
                     SQLTools.InsertSql("insert into photo (user_id,img_dir,activity_id) value(%s,'%s',%s)"%(user_id,img_dir,activity_id))
+            SQLTools.UpdateSql("update activity set status=2 where activity_id=%s" % activity_id)
+            SQLTools.UpdateSql("update activity set content='%s' where activity_id=%s" % (content, activity_id))
             return LittleTools.MakeJson(200,"")
         except Exception,e:
             print e

2d2056a6ff1c8797c90b5548e5daa1e329279b4c
TerenceLiu2 <741318489@qq.com>
2018-06-07 21:05:31 +0800

    all Project

--- GraphGo/Tools/SQLTools.py
+++ GraphGo/Tools/SQLTools.py
@@ -64,12 +64,20 @@ def GetHistoryFromSql(type,page_num,item_num,user_id):
         else:
             cur.execute("select activity.user_id,content,username,activity_id,status from activity,user where (status=0 or status=1) and user.user_id=activity.user_id and user.user_id=%s limit %d,%d ;"%(user_id,(page_num-1)*item_num,item_num))
             list = cur.fetchall()
+            cur.execute(
+                "select activity.user_id,content,username,activity_id,status from activity,user where (status=0 or status=1) and user.user_id=activity.user_id and attender_id=%s limit %d,%d ;" % (
+                user_id, (page_num - 1) * item_num, item_num))
+            list.append(cur.fetchall())
             conn.close()
             return list
     elif type==1:
         if page_num==0:
             cur.execute("select activity.user_id,content,username,activity_id,status from activity,user where status=2 and user.user_id=activity.user_id and user.user_id=%s;"%user_id)
             list = cur.fetchall()
+            cur.execute(
+                "select activity.user_id,content,username,activity_id,status from activity,user where status=2 and user.user_id=activity.user_id and attender_id=%s limit %d,%d ;" % (
+                    user_id, (page_num - 1) * item_num, item_num))
+            list.append(cur.fetchall())
             conn.close()
             return list
         else:
